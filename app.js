628cbe6f80d0ff5caac0de57d977d188610093bb
Alyson T <shtakai@gmail.com>
2017-09-10 15:23:36 +0900

    add writing up weather search

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
+require('dotenv').config();
 const yargs = require('yargs');
-
 const geocode = require('./geocode/geocode');
+const darkskyApiKey = process.env.DARKSKY_API_KEY
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true,
-    },
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true,
+//     },
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+const request = require('request');
+
+request({
+  url: `https://api.darksky.net/forecast/${darkskyApiKey}/39,-75`,
+  json: true,
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('unnable to fetch weather.');
   }
 });
+
