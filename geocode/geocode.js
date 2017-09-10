4d2d9e5c41fcfb0556c38a81010beeec7e6c78f6
Alyson T <shtakai@gmail.com>
2017-09-10 14:55:30 +0900

    add abstracting calalbacks

---
+++ geocode/geocode.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+const geocodeAddress = (address, callback) => {
+  const encodedAddress = encodeURIComponent(address);
+
+  request({
+    url: `https://maps.google.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true,
+  }, (error, response, body) => {
+    if (error) {
+      callback('unnable to connect to google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+      callback('unnable to find that address');
+    } else if (body.status === 'OK') {
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latittude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng,
+      });
+    }
+  });
+};
+
+module.exports = {
+  geocodeAddress,
+};
+
