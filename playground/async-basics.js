596bbb1bc1cc373ceb72436cc10def2926f81a6c
Alyson T <shtakai@gmail.com>
2017-09-10 12:45:30 +0900

    add async basics

---
+++ playground/async-basics.js
@@ -0,0 +1,13 @@
+console.log('starting app');
+
+setTimeout(() => {
+  console.log('inside of callback');
+}, 2000);
+
+
+setTimeout(() => {
+  console.log('---inside of callback');
+}, 0);
+
+
+console.log('finishing app');
