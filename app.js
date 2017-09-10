c41e10da6ab04eb411570b407cb33080dda8603e
Alyson T <shtakai@gmail.com>
2017-09-10 14:24:41 +0900

    add encoding user input

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true,
+    },
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.google.com/maps/api/geocode/json?address=wad%20ched%20yod',
+  url: `https://maps.google.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true,
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
