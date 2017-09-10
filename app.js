e04909fbb3dbf1c841579ca5d4f675387d39cfee
Alyson T <shtakai@gmail.com>
2017-09-10 14:03:34 +0900

    add http request

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
 request({
-  url: 'http://maps.google.com/maps/api/geocode/json?address=wad%20ched%20yod',
+  url: 'https://maps.google.com/maps/api/geocode/json?address=wad%20ched%20yod',
   json: true,
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`lat: ${body.results[0].geometry.location.lat}`);
+  console.log(`lng: ${body.results[0].geometry.location.lng}`);
 });
